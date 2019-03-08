fb0b0353176df7d5e63db98932e849edaed1b5e0
Cameron Fincher <camfincher@hotmail.com>
2019-03-08 12:46:10 -0600

    just another small update

--- Exploring Code.R
+++ Exploring Code.R
@@ -2,6 +2,7 @@
 library(MASS) #for LDA
 library(tidyverse)
 library(Boruta)
+library(pROC)
 
 # Reading in the files
 df = read.csv('Data/train.csv')
@@ -33,13 +34,77 @@ df_boruta = Boruta(target ~., data = df[,-1] %>%
 
 getSelectedAttributes(df_boruta)
 
-borruta_Var = attStats(df_boruta) %>% rownames_to_column() %>% arrange(desc(meanImp)) %>% slice(1:10) %>% pull()
+borruta_Var = attStats(df_boruta) %>% rownames_to_column() %>% arrange(desc(meanImp)) %>% slice(1:140) 
 
 
-df
 #let's try LDA to see if we can split the diffrence
 
 df_lda = lda(target ~., data = df[,c('target',borruta_Var$rowname)] %>% 
                                 mutate(target = factor(target)) %>%
                                 group_by(target) %>%
-                                sample_n(10000))
+                                sample_n(40000, replace = T) %>% 
+                                ungroup() %>%
+                                select_(quo(borruta_Var[1:40,]$rowname), 'target')
+               )
+df_lda_pred = (as.integer(predict(df_lda, newdata = df)[[1]]) - 1)
+
+sqrt(mean((df$target - df_lda_pred)^2))
+
+roc(df$target, df_lda_pred)
+
+df_roc = data.frame(var = 1:140, roc = 0, RMSE = 0)
+
+for (x in 1:nrow(df_roc)) {
+  print(x)
+  df_lda = lda(target ~., data = df[,c('target',borruta_Var$rowname)] %>% 
+                 mutate(target = factor(target)) %>%
+                 group_by(target) %>%
+                 sample_n(40000, replace = T) %>% 
+                 ungroup() %>%
+                 select_(quo(borruta_Var[1:df_roc[x,1],]$rowname), 'target')
+          )
+               
+      df_lda_pred = (as.integer(predict(df_lda, newdata = df)[[1]]) - 1)
+               
+      df_roc[x,2] = as.numeric(roc(df$target, df_lda_pred)$auc)
+      df_roc[x,3] = sqrt(mean((df$target - df_lda_pred)^2))
+
+}
+
+
+ggplot(data = df_roc, aes(x = var, y = RMSE)) + geom_line()
+
+df_qda = qda(target ~., 
+             data = df[,c('target',borruta_Var$rowname)] %>% 
+               mutate(target = factor(target)) %>%
+               group_by(target) %>%
+               sample_n(20000) %>% 
+               ungroup() %>%
+               select_(quo(borruta_Var[1:180,]$rowname), 'target') 
+)
+
+df_qda_pred = (as.integer(predict(df_qda, newdata = df)[[1]]) - 1)
+
+sqrt(mean((df$target - df_qda_pred)^2))
+
+
+
+roc(df$target, df_qda_pred)
+
+
+df_log = glm(target ~., 
+             data = df[,c('target',borruta_Var$rowname)] %>% 
+               mutate(target = factor(target)) %>%
+               group_by(target) %>%
+               sample_n(20000) %>% 
+               ungroup() %>%
+               select_(quo(borruta_Var[1:40,]$rowname), 'target'),
+             family = 'binomial')
+
+
+df_log_pred = ifelse(predict(df_log, newdata = df) > 0.5, 1, 0)
+
+sqrt(mean((df$target - df_log_pred)^2))
+
+
+roc(df$target, df_log_pred)
